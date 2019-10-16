b41d33173b983045359ec22ff0dba068be6b60e7
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-16 11:36:12 +0000

    Snapshot of branch develop taken on 2019-10-16T07:36:10.718-04:00 from "https://github.com/UnofficialJuliaMirror/FEMMaterials.jl-0c6c1f6c-65b6-54d6-9cff-6b64003af638"

---
+++ test/runtests.jl
@@ -0,0 +1,25 @@
+# This file is a part of JuliaFEM.
+# License is MIT: see https://github.com/JuliaFEM/Materials.jl/blob/master/LICENSE
+
+using FEMBase, Materials, FEMMaterials, Test
+
+@testset "Test FEMMaterials.jl" begin
+    @testset "test simulator" begin
+        include("test_simulator.jl")
+    end
+    @testset "test mechanical material solver" begin
+        include("test_mecamatso.jl")
+    end
+    @testset "test ideal plastic material model with shear strain" begin
+        include("test_idealplastic_shear.jl")
+    end
+    @testset "test chaboche material model" begin
+        include("test_chaboche.jl")
+    end
+    @testset "test chaboche material model with shear strain" begin
+        include("test_chaboche_shear.jl")
+    end
+    @testset "test viscoplastic" begin
+        include("test_viscoplastic.jl")
+    end
+end
